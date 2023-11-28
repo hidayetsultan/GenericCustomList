d7c9bc36024e6b4ea22ceb46cca59d43bff089b5
hidayetsultan <148267504+hidayetsultan@users.noreply.github.com>
2023-11-28 20:13:51 +0400

    Add files via upload

---
+++ Generic Customlist/Generic Customlist/CustomList.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class CustomList<T>
+{
+    private List<T> items;
+    private int capacityIncrement;
+
+    public CustomList(int initialCapacity = 0, int capacityIncrement = 8)
+    {
+        this.items = new List<T>(initialCapacity);
+        this.capacityIncrement = capacityIncrement;
+    }
+
+    public int Count
+    {
+        get { return items.Count; }
+    }
+
+    public int Capacity
+    {
+        get { return items.Capacity; }
+    }
+
+    public void Add(T item)
+    {
+        items.Add(item);
+
+        if (Count == Capacity)
+        {
+            items.Capacity += capacityIncrement;
+        }
+    }
+
+    public T Find(Predicate<T> match)
+    {
+        return items.Find(match);
+    }
+
+    public List<T> FindAll(Predicate<T> match)
+    {
+        return items.FindAll(match);
+    }
+
+    public bool Contains(T item)
+    {
+        return items.Contains(item);
+    }
+
+    public bool Exists(Predicate<T> match)
+    {
+        return items.Exists(match);
+    }
+
+    public bool Remove(T item)
+    {
+        bool removed = items.Remove(item);
+
+        if (removed && Count + capacityIncrement < Capacity)
+        {
+            items.Capacity -= capacityIncrement;
+        }
+
+        return removed;
+    }
+}
---
+++ Generic Customlist/Generic Customlist/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Example usage
+        CustomList<int> customList = new CustomList<int>(capacityIncrement: 8);
+
+        customList.Add(1);
+        customList.Add(2);
+        customList.Add(3);
+
+        Console.WriteLine($"Count: {customList.Count}, Capacity: {customList.Capacity}");
+
+        customList.Remove(2);
+
+        Console.WriteLine($"Count: {customList.Count}, Capacity: {customList.Capacity}");
+    }
+}
